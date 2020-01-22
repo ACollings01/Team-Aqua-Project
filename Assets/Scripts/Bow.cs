b4e04523aa5a684b7ebe598115d4b064bda4ee87
Tanner Olsen [Student] <tanner.olsen@student.sl.on.ca>
2020-01-22 11:10:54 -0500

    added in bow and ranged weapons scripts to allow shooting. Still primative

---
+++ Assets/Scripts/Bow.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bow : RangedWeapons
+{
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0) && (Time.time - lastFireTime) > fireRate)
+        {
+            lastFireTime = Time.time;
+            Fire();
+        }
+    }
+}
---
+++ Assets/Scripts/RangedWeapons.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class RangedWeapons : MonoBehaviour
+{
+
+    public float fireRate;
+    public int range;
+    public int damage;
+    protected float lastFireTime;
+    public GameObject arrowPrefab;
+    public Transform launchPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lastFireTime = Time.time - 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    protected void Fire()
+    {
+
+        GameObject arrow = Instantiate(arrowPrefab) as GameObject;
+
+        arrow.transform.position = launchPosition.position;
+
+        arrow.GetComponent<Rigidbody>().velocity = transform.parent.forward * 20;
+
+        //Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+        //RaycastHit hit;
+        //if (Physics.Raycast(ray, out hit, range))
+        //{
+        //    processHit(hit.collider.gameObject);
+        //}
+    }
+
+    private void processHit(GameObject hitObject)
+    {
+        //if (hitObject.GetComponent<Player>() != null)
+        //{
+        //    hitObject.GetComponent<Player>().TakeDamage(damage);
+        //}
+        //if (hitObject.GetComponent<Bat>() != null)
+        //{
+        //    hitObject.GetComponent<Robot>().TakeDamage(damage);
+        //}
+    }
+}
