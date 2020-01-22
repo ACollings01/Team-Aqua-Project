9e39e3a8089b57a637d4ddae741e36a98e0fd9b1
Andrew Scarlett [Student] <anscarlett@rogers.com>
2020-01-22 11:12:36 -0500

    Created a personal AI Scene for testing, created a working Bat AI prefab

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,6 +7,8 @@ public class Player : MonoBehaviour
     public Game game;
     public float health;
     public float armour;
+    public int speed;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -27,6 +29,15 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        transform.Translate(movement * speed * Time.deltaTime);
+    }
 
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Projectile")
+        {
+            Destroy(other.gameObject);
+        }
     }
 }
