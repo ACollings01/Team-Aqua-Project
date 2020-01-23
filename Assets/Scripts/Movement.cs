0eaf10bfefa698e2f3d4788c1d2ad85cbe731f32
Colin Labelle [Student] <colin.labelle@student.sl.on.ca>
2020-01-23 14:23:56 -0500

    Added joystick movement scripts

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    public Joystick joystick;
+    public float speed = 3.0f;
+
+    private float startTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        joystick = FindObjectOfType<Joystick>();
+
+        startTime = 0.0f;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var rigidbody = GetComponent<Rigidbody>();
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            startTime = Time.time;
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            startTime = 0.0f;
+           // Debug.Log("startTime" + startTime);
+        }
+
+        if (Time.time > startTime + 0.25f && startTime != 0.0f)
+        {
+            //Debug.Log("startTime" + startTime);
+            rigidbody.velocity = new Vector3(joystick.Horizontal * speed, rigidbody.velocity.y, joystick.Vertical * speed);
+
+        }
+        else
+        {
+            rigidbody.velocity = new Vector3(0,0,0);
+        }
+
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        transform.Translate(movement * speed * Time.deltaTime);
+    }
+}
