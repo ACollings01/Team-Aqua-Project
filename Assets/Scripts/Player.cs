d4d40d6c49859ecb10658634896ee6786250278b
Colin Labelle [Student] <colin.labelle@student.sl.on.ca>
2020-01-27 15:43:33 -0500

    Added a scene for Colin's stuff and transfered my movement overto it

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,12 +7,17 @@ public class Player : MonoBehaviour
     public Game game;
     public float health;
     public float armour;
-    public int speed;
+    public float speed;
+    public Joystick joystick;
+
+    private float startTime;
 
     // Start is called before the first frame update
     void Start()
     {
+        joystick = FindObjectOfType<Joystick>();
 
+        startTime = 0.0f;
     }
 
     public void TakeDamage(float amount)
@@ -29,6 +34,30 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        var rigidbody = GetComponent<Rigidbody>();
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            startTime = Time.time;
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            startTime = 0.0f;
+            // Debug.Log("startTime" + startTime);
+        }
+
+        if (Time.time > startTime + 0.25f && startTime != 0.0f)
+        {
+            //Debug.Log("startTime" + startTime);
+            rigidbody.velocity = new Vector3(joystick.Horizontal * speed, rigidbody.velocity.y, joystick.Vertical * speed);
+
+        }
+        else
+        {
+            rigidbody.velocity = new Vector3(0, 0, 0);
+        }
+
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         transform.Translate(movement * speed * Time.deltaTime);
     }
