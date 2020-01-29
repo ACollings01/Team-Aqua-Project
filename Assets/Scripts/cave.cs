397cd1f61b6bf7958e14091e033726dc948386e8
TimWoods77 <timw78695@gmail.com>
2020-01-29 11:44:54 -0500

    made a audiomanager script, cave script, forest script, gamesounds script, and added FMod to project

---
+++ Assets/Scripts/cave.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cave : MonoBehaviour
+{
+    [FMODUnity.EventRef]
+    public string inputsound;
+    bool windincave;
+    bool waterdrips;
+    bool bubblinglava;
+
+    void Update()
+    {
+
+    }
+}
