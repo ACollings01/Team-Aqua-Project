ed5305ea78b9b398b93fbc5da813b4d68979b35a
Alexander Collings [Student] <alexander.collings@student.sl.on.ca>
2020-01-30 09:38:29 -0500

    Added a singleton bsae script along with a game manager and a magic circle for testing purposes

---
+++ Assets/Managers/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+
+public class GameManager : Singleton<GameManager>
+{
+    protected GameManager() { }
+
+    private static GameObject _player;
+
+    void Start()
+    {
+        _player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Returns the player GameObject
+    public GameObject GetPlayer()
+    {
+        return _player;
+    }
+
+    // Script will be filled later, will activate when player steps on a magic circle
+    public void ActivateMCircle()
+    {
+        Debug.Log("Player Stepped On A Magic Circle");
+    }
+}
