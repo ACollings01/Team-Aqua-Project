ca4deb4531ea7f727f003b38ca52eef9a6a33529
Colin Labelle [Student] <colin.labelle@student.sl.on.ca>
2020-02-06 14:20:24 -0500

    Optimizing weapon script

--- Assets/Scripts/Sword.cs
+++ Assets/Scripts/Sword.cs
@@ -1,113 +1,50 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEngine.EventSystems;
 using UnityEngine;
 
-public class Sword : MonoBehaviour
+public class Sword : Weapons
 {
     private Animator swordAnimator;
-    Collider swordCollider;
-    private bool quickTap = false;
-    private bool longTap = false;
-    private float startTime;
-    private int damage;
-
 
     bool AnimatorIsPlaying()
     {
         return swordAnimator.GetCurrentAnimatorStateInfo(0).normalizedTime < 1;
     }
 
-    // Start is called before the first frame update
     void Start()
     {
         GameObject sword = transform.gameObject;
         swordAnimator = sword.GetComponent<Animator>();
 
-        swordCollider = GetComponent<Collider>();
+        //swordCollider = GetComponent<Collider>();
 
         startTime = 0.0f;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (UnityEngine.EventSystems.EventSystem.current.IsPointerOverGameObject())
-        {
-            //Debug.Log("Tooch");
-        }
-        else
-        {
-            if (Input.GetMouseButtonDown(0))
-            {
-                startTime = Time.time;
-            }
-
-            if (Input.GetMouseButtonUp(0))
-            {
-                if (Time.time > startTime + 1.0f && startTime != 0.0f)
-                {
-                    longTap = true;
-                    quickTap = false;
-                }
-                else if (Time.time < startTime + 1.0f && startTime != 0.0f)
-                {
-                    quickTap = true;
-                    longTap = false;
-                }
-
-                startTime = 0;
-            }
-        }
+        lengthOfTap();
 
         if (quickTap && AnimatorIsPlaying())
         {
             swordAnimator.SetBool("Quick Tap", true);
-            swordCollider.enabled = true;
+            swordAttack();
         }
         else if (!AnimatorIsPlaying())
         {
             swordAnimator.SetBool("Quick Tap", false);
-            swordCollider.enabled = false;
             quickTap = false;
         }
 
         if (longTap && AnimatorIsPlaying())
         {
             swordAnimator.SetBool("Long Tap", true);
-            swordCollider.enabled = true;
+            swordAttack();
         }
         else if (!AnimatorIsPlaying())
         {
             swordAnimator.SetBool("Long Tap", false);
-            swordCollider.enabled = false;
             longTap = false;
         }
-
-
-        //for PC controls
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            startTime = Time.time;
-        }
-
-        if (Input.GetKeyUp(KeyCode.Space))
-        {
-            startTime = 0.0f;
-        }
-    }
-
-    void OnCollisionEnter(Collision enemy)
-    {
-        if (enemy.gameObject.tag == "Enemy")
-        {
-            enemy.gameObject.GetComponent<EnemyAI>().health -= swordDamageDone();
-        }
-    }
-
-    public int swordDamageDone()
-    {
-        damage = Random.Range(8, 11);
-        return damage;
     }
 }
--- Assets/Scripts/Weapons.cs
+++ Assets/Scripts/Weapons.cs
@@ -1,22 +1,78 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.EventSystems;
 using UnityEngine;
 
 public class Weapons : MonoBehaviour
 {
+    protected bool quickTap = false;
+    protected bool longTap = false;
+    protected float startTime;
+    protected int damage;
 
-    public float attackTime = 0.2f;
-    public float weaponDamage = 5.0f;
+    LayerMask whatIsEnemy;
+    float swordAttackRadius;
+    float spearAttackRadius;
+    float shieldAttackRadius;
 
-    // Start is called before the first frame update
     void Start()
+    {
+        whatIsEnemy = LayerMask.GetMask("Enemy");
+        swordAttackRadius = 1;
+    }
+
+    void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    protected void lengthOfTap()
     {
-        
+        if (UnityEngine.EventSystems.EventSystem.current.IsPointerOverGameObject())
+        {
+            //Debug.Log("Tooch");
+        }
+        else
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                startTime = Time.time;
+            }
+
+            if (Input.GetMouseButtonUp(0))
+            {
+                if (Time.time > startTime + 1.0f && startTime != 0.0f)
+                {
+                    longTap = true;
+                    quickTap = false;
+                }
+                else if (Time.time < startTime + 1.0f && startTime != 0.0f)
+                {
+                    quickTap = true;
+                    longTap = false;
+                }
+
+                startTime = 0;
+            }
+        }
+    }
+
+    protected void swordAttack()
+    {
+       GameObject playerSword = GameObject.Find("Player/Player_Model/Sword");
+
+       Collider[] enemyHit = Physics.OverlapSphere(playerSword.transform.position, swordAttackRadius, whatIsEnemy);
+
+       for (int i = 0; i < enemyHit.Length; i++)
+       {
+           enemyHit[i].GetComponent<EnemyAI>().health -= swordDamageDone();
+           Debug.Log("The player has hit the enemy!");
+       }
+    }
+
+    public int swordDamageDone()
+    {
+        damage = Random.Range(8, 11);
+        return damage;
     }
 }
