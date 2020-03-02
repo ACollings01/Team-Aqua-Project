72296ed17bfa390c29a4567ff8c1dd9f1d3d4a16
Colin Labelle [Student] <colin.labelle@student.sl.on.ca>
2020-03-02 16:22:06 -0500

    updated staff script

---
+++ Assets/Scripts/MagicBallDamage.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagicBallDamage : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnCollisionEnter(Collision enemy)
+    {
+        if (enemy.gameObject.tag == "Enemy")
+        {
+            enemy.gameObject.GetComponent<EnemyAI>().health -= magicDamageDone();
+        }
+    }
+
+    public int magicDamageDone()
+    {
+        int damageMagic = Random.Range(10, 16);
+        return damageMagic;
+    }
+}
