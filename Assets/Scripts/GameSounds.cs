33cde07ee9ad7f7a4648d409b5b562ffc5a23103
Wildthing77 <55107076+Wildthing77@users.noreply.github.com>
2020-03-04 09:05:35 -0500

    made changes
    made new soundmanager script added code to gamesounds script and added yaSingleton asset

--- Assets/Scripts/GameSounds.cs
+++ Assets/Scripts/GameSounds.cs
@@ -1,18 +1,27 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using UnityEditor;
 using UnityEngine;
 
+[System.Serializable]
 public class GameSounds : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public string audioName;
+    public AudioClip audioClip;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [Range(0f, 10f)]
+    public float volume = 5f;
+    [Range(0.5f, 1.5f)]
+    public float pitch = 1f;
+
+    [Range(0f, 5f)]
+    public float randvolume = 0.1f;
+    [Range(0f, 0.5f)]
+    public float randpitch = 0.1f;
+
+    [HideInInspector]
+
+    public AudioSource source;
+
+    public bool isLooping;
+
+    public bool playOnAwake;
 }
--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -1,66 +1,27 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
-[System.Serializable]
-public class Sound
+
+public class SoundManager : MonoBehaviour
 {
-    public string name;
-    public AudioClip clip;
+    public static SoundManager Instance;
+    public GameSounds[] gameSounds;
+    private float timeToReset;
+    private bool timerIsSet = false;
+    private float tempName;
+    private float tempVolume;
+    private bool isLowered = false;
+    private bool fadeOut = false;
+    private bool fadeIn = false;
+    private string fadeInUsedString;
 
-    [Range(0f, 10f)]
-    public float volume = 5f;
-    [Range(0.5f, 1.5f)]
-    public float pitch = 1f;
-
-    [Range(0f, 5f)]
-    public float randvolume = 0.1f;
-    [Range(0f, 0.5f)]
-    public float randpitch = 0.1f;
-
-    private AudioSource soundeffectaudio;
-
-    public void SetSoundEffectAudio(AudioSource _soundeffectaudio)
-    {
-        soundeffectaudio = _soundeffectaudio;
-        soundeffectaudio.clip = clip;
-    }
-
-    public void Play()
-    {
-        soundeffectaudio.volume = volume * (1 + Random.Range(-randvolume / 2f, randvolume / 2f));
-        soundeffectaudio.pitch = pitch * (1 + Random.Range(-randpitch / 2f, randpitch / 2f));
-        soundeffectaudio.Play();
-    }
-
-}
-
-
-public class SoundManager : Singleton<SoundManager>
-{
-    public static SoundManager Instance = null;
-    private AudioSource soundEffectAudio;
-
-    [SerializeField]
-    Sound[] sounds;
-
-    public AudioClip ES_Walk_Forest_Trail;
-    public AudioClip ES_Volcanoe_Lava_Bubbles;
-    public AudioClip Ambiances_Cave_Level;
-    public AudioClip ES_Dungeon_Dream_Cave;
-    public AudioClip Fire_Staff_Fireball_hit;
-    public AudioClip Fire_Staff_Woosh;
-    public AudioClip Ice_Staff_Hit_1;
-    public AudioClip Ice_Staff_Swoosh_1;
-    public AudioClip Lightning_Staff_Lightning_Strike;
-    public AudioClip Wolf_Howl;
-    public AudioClip Wolf_Bite_2;
-    public AudioClip Wolf_Damage_2;
-    public AudioClip Wolf_Death_3;
 
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         if (Instance == null)
         {
@@ -69,25 +30,146 @@ public class SoundManager : Singleton<SoundManager>
         else if (Instance != this)
         {
             Destroy(gameObject);
-            AudioSource[] sources = GetComponents<AudioSource>();
-            foreach (AudioSource source in sources)
+        }
+
+        DontDestroyOnLoad(gameObject);
+
+        foreach (var s in gameSounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.audioClip;
+            s.source.volume = s.volume;
+            s.source.loop = s.isLooping;
+            if (s.playOnAwake)
             {
-                if (source.clip == null)
-                {
-                    soundEffectAudio = source;
-                }
+                s.source.Play();
             }
         }
     }
 
-    public void PlayOneShot(AudioClip clip)
+    public static void PlayMusic (String name)
     {
-        soundEffectAudio.PlayOneShot(clip);
+      GameSounds s = Array.Find(Instance.gameSounds, GameSounds => GameSounds.audioName == name);
+      if (s == null)
+      {
+          Debug.LogError("Sound name" + name + "not found!");
+
+          return;
+      }
+      else
+      {
+       s.source.Play();
+      }
     }
 
-    // Update is called once per frame
-    void Update()
+    public static void StopMusic(String name)
     {
-        
+        GameSounds s = Array.Find(Instance.gameSounds, GameSounds => GameSounds.audioName == name);
+        if (s == null)
+        {
+            Debug.LogError("Sound name" + name + "not found!");
+
+            return;
+        }
+        else
+        {
+            s.source.Stop();
+        }
     }
-}
+
+    public static void PauseMusic(String name)
+    {
+        GameSounds s = Array.Find(Instance.gameSounds, GameSounds => GameSounds.audioName == name);
+        if (s == null)
+
+        {
+
+            Debug.LogError("Sound name" + name + "not found!");
+
+            return;
+
+        }
+
+        else
+
+        {
+
+            s.source.Pause();
+
+        }
+    }
+
+    public static void UnPauseMusic(String name)
+    {
+        GameSounds s = Array.Find(Instance.gameSounds, GameSounds => GameSounds.audioName == name);
+        if (s == null)
+
+        {
+
+            Debug.LogError("Sound name" + name + "not found!");
+
+            return;
+
+        }
+
+        else
+
+        {
+
+            s.source.UnPause();
+
+        }
+    }
+
+    public static void LowerVolume(String name, float _duration)
+
+    {
+
+        if (Instance.isLowered == false)
+
+        {
+            GameSounds s = Array.Find(Instance.gameSounds, GameSounds => GameSounds.audioName == name);
+            if (s == null)
+
+            {
+
+                Debug.LogError("Sound name" + name + "not found!");
+
+                return;
+
+            }
+
+            else
+            {
+                Instance.tempName = name;
+                Instance.tempVolume = s.volume;
+                Instance.timeToReset = Time.time + _duration;
+                Instance.timerIsSet = true;
+                s.source.volume = s.source.volume / 5;
+
+            }
+
+            Instance.isLowered = true;
+        }
+    }
+
+    public static void IFadeOut(String name, float duration)
+
+    {
+
+        Instance.StartCoroutine(Instance.IFadeOut(name, duration));
+
+    }
+
+    public static void FadeIn(String name, float targetVolume, float duration)
+
+    {
+
+        instance.StartCoroutine(instance.IFadeIn(name, targetVolume, duration));
+
+    }
+
+
+
+
+
