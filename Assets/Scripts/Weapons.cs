193bc80a76e3c445f3b17daa5e50a6c1132379aa
Colin Labelle <colinlabelle777@gmail.com>
2020-03-26 05:27:44 -0400

    reverted to last working controls build

--- Assets/Scripts/Weapons.cs
+++ Assets/Scripts/Weapons.cs
@@ -45,17 +45,12 @@ public class Weapons : MonoBehaviour
         }
         else
         {
-            if (Input.touchCount > 0)
-            {
-                if (Input.GetTouch(0).phase == TouchPhase.Began /*|| Input.GetTouch(1).phase == TouchPhase.Began || Input.GetMouseButtonDown(0)*/)
+                if (Input.GetMouseButtonDown(0))
                 {
                     startTime = Time.time;
                 }
-            }
 
-            if (Input.touchCount > 0)
-            {
-                if (Input.GetTouch(0).phase == TouchPhase.Ended /*|| Input.GetTouch(1).phase == TouchPhase.Ended || Input.GetMouseButtonUp(0)*/)
+                if (Input.GetMouseButtonUp(0))
                 {
                     if (Time.time > startTime + 0.5f && startTime != 0.0f)
                     {
@@ -70,7 +65,6 @@ public class Weapons : MonoBehaviour
 
                     startTime = 0;
                 }
-            }
         }
     }
 
