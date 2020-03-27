6bb125d5c93e497b3bc143fcfd67a511a25b42a9
Colin Labelle <colinlabelle777@gmail.com>
2020-03-26 21:16:52 -0400

    added touch controls (test 21)

--- Assets/Scripts/Sword.cs
+++ Assets/Scripts/Sword.cs
@@ -27,23 +27,28 @@ public class Sword : Weapons
 
     void Update()
     {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
-
-        layerMask = ~layerMask;
-
-        lengthOfTap();
-
-        if (Physics.Raycast(ray, out hit, 1000, ignoreLayerMask))
+        foreach (Touch touch in Input.touches)
         {
-            lookAtClick = lookAtClick;
-        }
-        else if (quickTap == false)
-        {
-            if (Physics.Raycast(ray, out hit, 1000, layerMask))
+            Ray ray = Camera.main.ScreenPointToRay(touch.position);
+        
+            RaycastHit hit;
+
+            layerMask = ~layerMask;
+
+            lengthOfTap();
+
+            if (Physics.Raycast(ray, out hit, 1000, ignoreLayerMask))
             {
-                lookAtClick = new Vector3(hit.point.x, hit.point.y + 1.1f, hit.point.z);
+                lookAtClick = lookAtClick;
             }
+            else if (quickTap == false)
+            {
+                if (Physics.Raycast(ray, out hit, 1000, layerMask))
+                {
+                    lookAtClick = new Vector3(hit.point.x, hit.point.y + 1.1f, hit.point.z);
+                }
+            }
+
         }
 
         if (quickTap /*&& AnimatorIsPlaying()*/)
