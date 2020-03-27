86124c00ad2e1a086424c037a68ef016be5454c0
Colin Labelle <colinlabelle777@gmail.com>
2020-03-26 20:00:43 -0400

    added touch controls (test 13)

--- Assets/Scripts/Weapons.cs
+++ Assets/Scripts/Weapons.cs
@@ -50,12 +50,12 @@ public class Weapons : MonoBehaviour
             {
                 foreach (Touch touch in Input.touches)
                 {
-                    if (Input.GetTouch(0).phase == TouchPhase.Began || Input.GetTouch(1).phase == TouchPhase.Began/*Input.GetMouseButtonDown(0)*/)
+                    if (touch.phase == TouchPhase.Began/*Input.GetMouseButtonDown(0)*/)
                     {
                         startTime = Time.time;
                     }
 
-                    if (Input.GetTouch(0).phase == TouchPhase.Ended || Input.GetTouch(1).phase == TouchPhase.Ended/*Input.GetMouseButtonUp(0)*/)
+                    if (touch.phase == TouchPhase.Ended/*Input.GetMouseButtonUp(0)*/)
                     {
                         if (Time.time > startTime + 0.5f && startTime != 0.0f)
                         {
