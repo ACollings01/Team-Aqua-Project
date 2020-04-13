f9bbdff35b2d424cf505e3e1830d8ee8b7fc8766
Brittany <bcryderman2322@gmail.com>
2020-04-13 01:39:51 -0400

    level two button

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -11,6 +11,11 @@ public class Menu : MonoBehaviour
     {
         SceneManager.LoadScene("Level 1");
     }
+
+    public void LevelTwo()
+    {
+        SceneManager.LoadScene("Relevel2");
+    }
     
     public void TownScene()
     {
