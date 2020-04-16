7c44e5d1149d932966c288069f8a2affda1b4f11
Andrew Scarlett <anscarlett@rogers.com>
2020-04-16 13:01:18 -0400

    Fixed enemies proving the player with endless amounts of money on death

--- Assets/Scripts/EnemySC's/Necromancer/NecromancerSc.cs
+++ Assets/Scripts/EnemySC's/Necromancer/NecromancerSc.cs
@@ -7,6 +7,8 @@ public class NecromancerSc : EnemyAI
 {
     public GameObject inv;
     bool spawned = false;
+    private bool isDead;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -24,8 +26,12 @@ public class NecromancerSc : EnemyAI
 
         if (this.health <= 0)
         {
-            inv.GetComponent<DisplayInventory>().inventory.Container[0].AddAmount(100);
-            Destroy(this.gameObject);
+            if (!isDead)
+            {
+                inv.GetComponent<DisplayInventory>().inventory.Container[0].AddAmount(100);
+                Destroy(this.gameObject, 2f);
+            }
+            isDead = true;
         }
 
     }
